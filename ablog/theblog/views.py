68cd7f8d1e40bf28dbac726dd909cb62a2eeff7e
Claire Munyole <munyolec@gmail.com>
2021-05-17 10:28:46 +0300

    a simple blog

---
+++ ablog/theblog/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from.models import Post, Category
+from .forms import PostForm,EditForm
+from django.urls import reverse_lazy
+
+
+class HomeView(ListView):
+    model=Post
+    template_name='home.html'
+    ordering=['-post_date']
+
+class ArticleDetailView(DetailView):
+    model=Post
+    template_name='article_detail.html'
+
+class AddPostView(CreateView):
+    model=Post
+    form_class=PostForm
+    template_name='add_post.html'
+
+class AddCategoryView(CreateView):
+    model=Category
+    template_name='add_category.html'
+    fields=["name"]    
+    
+class UpdatePostView(UpdateView):
+    model=Post
+    form_class=EditForm
+    template_name='update_post.html'
+
+
+class DeletePostView(DeleteView):
+    model=Post
+    template_name='delete_post.html'
+    success_url=reverse_lazy('home')
+
+def CategoryView(request, cats):
+    category_posts=Post.objects.filter(category=cats.replace('-',' '))
+    print(cats)
+    return render(request,'categories.html', {'cats':cats.title().replace('-',' '),'category_posts':category_posts})
