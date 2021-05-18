cb452e10b8e4ca3f2d9e7a4265e6d739c82ad604
munyolec@gmail.com <munyolec@gmail.com>
2021-05-18 15:24:51 +0300

    Additional registration fields

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -28,8 +28,15 @@ class ArticleDetailView(DetailView):
         stuff=get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes=stuff.total_likes()
 
+        liked=False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked=True
+
+
         context["cat_menu"]=cat_menu
         context["total_likes"]=total_likes
+        context["liked"]=liked
+
         return context
 
 class AddPostView(CreateView):
@@ -63,7 +70,14 @@ def CategoryListView(request):
 
 def LikeView(request, pk):
     post=get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked=False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked=False
+    else:
+        post.likes.add(request.user)
+        liked=True
+
     return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
     
 
