b0b65cdb6e2b5f8aeb02e09c6fba0449845efe74
munyolec@gmail.com <munyolec@gmail.com>
2021-05-18 15:51:07 +0300

    rich text editor

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime,date
-from ckeditor.fields import RichTexField
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Category(models.Model):
@@ -19,7 +19,7 @@ class Post(models.Model):
     title=models.CharField(max_length=255)
     title_tag=models.CharField(max_length=255)
     author=models.ForeignKey(User, on_delete=models.CASCADE)
-    body=RichTexField(blank=True, null=True)
+    body=RichTextField(blank=True, null=True)
     # body=models.TextField()
     post_date=models.DateField(auto_now_add=True)
     category=models.CharField(max_length=255, default='no category')
