b35ca6fc8d774221ec607cb2287367503ce0f6c5
munyolec@gmail.com <munyolec@gmail.com>
2021-05-24 10:10:26 +0300

    change user password page

--- ablog/ablog/urls.py
+++ ablog/ablog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('theblog.urls')),
     path('members/',include('django.contrib.auth.urls')),
-    path('members',include('members.urls'))
+    path('members/',include('members.urls')),
 
 
 ]
