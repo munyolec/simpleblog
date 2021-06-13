2c3ef574d99d2e992bdcb0d907b52eb550a5e55e
munyolec@gmail.com <munyolec@gmail.com>
2021-06-13 17:24:25 +0300

    default profile pic with static files

--- ablog/ablog/settings.py
+++ ablog/ablog/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL= '/media/'
 
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
 STATICFILES_DIRS = (
